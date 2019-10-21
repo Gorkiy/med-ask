57e1f832cbcb66eebbe5c26651949359d1181b17
Peter Yakovlev <gorkiy@gmail.com>
2019-10-21 15:59:29 +0300

    Updates

---
+++ precache-manifest.a780a8da81a836e01219ec49cf481014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470cdb93912666cc17cb7ec09ab38eae",
+    "url": "/med-ask/index.html"
+  },
+  {
+    "revision": "2e4ffc7676f39e237068",
+    "url": "/med-ask/static/css/main.53d7f580.chunk.css"
+  },
+  {
+    "revision": "898f2a0207d8039de1a9",
+    "url": "/med-ask/static/js/2.550dea86.chunk.js"
+  },
+  {
+    "revision": "2e4ffc7676f39e237068",
+    "url": "/med-ask/static/js/main.88bab81d.chunk.js"
+  },
+  {
+    "revision": "30f97da2f6d79df2539a",
+    "url": "/med-ask/static/js/runtime-main.e4cbcbb9.js"
+  },
+  {
+    "revision": "59f9ddd558040fec4394c79897685e8a",
+    "url": "/med-ask/static/media/icon-delete.59f9ddd5.svg"
+  }
+]);
